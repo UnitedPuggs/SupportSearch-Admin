2a34f61b90dcfd7617894f620d080a609c6fbb14
Eddie <edpoulsonv@hotmail.com>
2022-12-19 16:04:47 -0800

    testing reactivity with supabase

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { createClient } from "@supabase/supabase-js";
+	import { prevent_default } from "svelte/internal";
+    const supabaseUrl = 'https://gzytpbcundzfdkivabpf.supabase.co'
+    const supabaseKey = '1';
+    const supabase = createClient(supabaseUrl, supabaseKey)    
+
+    let email;
+    let user;
+    async function verifyUser(email) {
+        const { data, error } = await supabase
+        .from('users')
+        .select('email')
+        .eq('email', email)
+
+        if(data) {
+            user = data.email
+        }
+        
+        if(error) {
+            console.log(error)
+        }
+    }
+</script>
+
+<main>
+    Welcome to the SupportSearch Admin Page!
+</main>
+
+<input type="text" placeholder="email" bind:value={email} required/>
+
+{#if email}
+    <button on:click={ verifyUser(email) }>Login</button>
+    <h1>{user}</h1>
+{/if}
