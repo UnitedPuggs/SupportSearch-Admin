3ddafea9c620a5268373d459cc4983d3cdb16716
UnitedPuggs <edpoulsonv@hotmail.com>
2022-12-25 23:26:38 -0800

    feat: license search generates buttons for licenses that show something (in the console)

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,5 +1,3 @@
-import { redirect } from "@sveltejs/kit";
-
 export async function load({ cookies }) {
     const sessionId = cookies.get('session');
 
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -3,8 +3,9 @@
     import { page } from "$app/stores";
 
     let licensedata;
+    let info;
     let menuon = false;
-    let menuoptions = ["Requests", "Users", "test"]
+    let menuoptions = ["Feature Requests", "Users"]
 
     export let license;
 
@@ -15,7 +16,34 @@
     async function getLicenses() {
         const response = await fetch("/dashboard?license=" + license);
         licensedata = await response.json();
-        console.log(licensedata["licenses"]);
+        console.log(licensedata);
+        licensedata = licensedata;
+    }
+
+    async function getCSTimingVers(license) {
+        fetch("https://" + license + ".clubspeedtiming.com/api/index.php/version/current.json?key=cs-dev")
+        .then(response => response.json())
+        .then(data => { 
+            info = data; 
+            console.log(info); 
+        })
+        .catch(err => {
+                console.log(err);
+                if(err.message.includes("Failed to fetch"))
+                    getCSVers(license);
+        })
+    }
+
+    async function getCSVers(license) {
+        fetch("https://" + license + ".clubspeed.com/api/index.php/version/current.json?key=cs-dev")
+        .then(response => response.json())
+        .then(data => { 
+            info = data; 
+            console.log(info); 
+        })
+        .catch(err => {
+            console.log(err);
+        })
     }
 </script>
 
@@ -25,10 +53,10 @@
 
 {#if $page.data.user}
 <div class="flex flex-row">
-<button class="my-5 ml-5 px-1.5 py-0.5 bg-black text-white drop-shadow-md hover:opacity-75 font-mono" on:click={ clickMenu }>Menu</button>
+<button class="my-5 ml-5 px-1 py-0.5 bg-black text-white drop-shadow-md hover:opacity-75 font-mono" on:click={ clickMenu }>Menu</button>
 {#if menuon}
     {#each menuoptions as option, i}
-        <button in:fly='{{ delay: 100 + (i * 100), duration: 200 }}' out:fly='{{ delay: 100 - (i * 100), duration: 200}}' class="px-2 my-5 bg-black text-white hover:opacity-75 font-mono">{option}</button>
+        <button in:fly='{{ delay: 100 + (i * 100), duration: 200 }}' out:fly='{{ delay: 100 - (i * 100), duration: 200}}' class="px-1 my-5 bg-black text-white hover:opacity-75 font-mono">{option}</button>
     {/each}
 {/if}
 </div>
@@ -44,6 +72,13 @@
                 <button transition:fly='{{ y:10, duration: 750 }}' class="bg-black rounded-lg text-white px-1 py-0.5 drop-shadow-md hover:opacity-75" on:click={ getLicenses }>Search</button>
             {/if}
         </form>
+        {#if licensedata && license}
+            <div class="flex flex-col pt-5 mr-10">
+            {#each licensedata["licenses"] as name}
+                <button class="hover:opacity-50" on:click={ getCSTimingVers(name.license) }>{name.license}</button>
+            {/each}
+            </div>
+        {/if}
     </div>
 </div>
 {:else}
