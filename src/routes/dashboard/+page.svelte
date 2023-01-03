2e8bbf3ed380fc740451e924071c192cdde0df5e
UnitedPuggs <edpoulsonv@hotmail.com>
2023-01-02 16:24:25 -0800

    wip: added license component on click, needs some interactability and some styling

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,16 +1,18 @@
 <script>
     import { fly, slide } from "svelte/transition";
     import { page } from "$app/stores";
+    import SiteInfoBox from "./SiteInfoBox.svelte";
+  import { get } from "svelte/store";
 
     let licensedata;
     let info;
-    let menuon = false;
+    let menuOn = false;
     let menuoptions = ["Feature Requests", "Users"]
 
-    export let license;
+    let license;
 
     function clickMenu() {
-        menuon = !menuon;
+        menuOn = !menuOn;
     }
 
     async function getLicenses() {
@@ -32,6 +34,7 @@
                 if(err.message.includes("Failed to fetch"))
                     getCSVers(license);
         })
+        document.createElement("SiteInfoBox");
     }
 
     async function getCSVers(license) {
@@ -45,6 +48,14 @@
             console.log(err);
         })
     }
+
+    function abc(name) {
+        const element = new SiteInfoBox({target: document.querySelector("#k1bluffton")})
+    }
+
+    function makeVis(name) {
+        document.getElementById(name).style.display = "block"
+    }
 </script>
 
 <svelte:head>
@@ -54,7 +65,7 @@
 {#if $page.data.user}
 <div class="flex flex-row">
 <button class="my-5 ml-5 px-1 py-0.5 bg-black text-white drop-shadow-md hover:opacity-75 font-mono" on:click={ clickMenu }>Menu</button>
-{#if menuon}
+{#if menuOn}
     {#each menuoptions as option, i}
         <button in:fly='{{ delay: 100 + (i * 100), duration: 200 }}' out:fly='{{ delay: 100 - (i * 100), duration: 200}}' class="px-1 my-5 bg-black text-white hover:opacity-75 font-mono">{option}</button>
     {/each}
@@ -67,15 +78,17 @@
             {#if !license}
                 <h1 class="my-5" transition:slide='{{ delay: 100, duration: 200 }}'>What are you looking for?</h1>
             {/if}
+
             <input class="px-1 border-black border-2 rounded-md focus:ring-1 ring-black ring-inset" type="text" placeholder="license name" bind:value={license}>
+            
             {#if license}
                 <button transition:fly='{{ y:10, duration: 750 }}' class="bg-black rounded-lg text-white px-1 py-0.5 drop-shadow-md hover:opacity-75" on:click={ getLicenses }>Search</button>
             {/if}
         </form>
         {#if licensedata && license}
-            <div class="flex flex-col pt-5 mr-10">
+            <div id="licenses" class="flex flex-col pt-5 mr-10">
             {#each licensedata["licenses"] as name}
-                <button class="hover:opacity-50" on:click={ getCSTimingVers(name.license) }>{name.license}</button>
+                <SiteInfoBox license={name.license} version={name.version} updated={name.updated} notes={name.notes} on:click={ getCSTimingVers(name.license), makeVis(name.license) }/>
             {/each}
             </div>
         {/if}
---
+++ src/routes/dashboard/SiteInfoBox.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let license;
+    export let version;
+    export let updated;
+    export let notes;
+
+</script>
+
+<button class="hover:opacity-50" on:click>{license}</button>
+
+<!-- 
+    Just make a more interactive 'box' that is visible once user clicks on a license name
+-->
+<div id={license} class="hidden">
+    <p>{license} is {version}</p>
+    <p>Updated on {updated}</p>
+    <p>Notes: {notes}</p>
+</div>
