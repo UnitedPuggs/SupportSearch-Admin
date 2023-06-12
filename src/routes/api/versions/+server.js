13cab7a5393be0b4fb8d7450feb09e5da687b8b0
Eddie <edpoulsonv@hotmail.com>
2023-06-12 10:15:47 -0700

    feat: added route for selecting by version

---
+++ src/routes/api/versions/+server.js
@@ -0,0 +1,22 @@
+import { supabase } from "$lib/supabaseClient"
+import { json } from "@sveltejs/kit";
+
+export async function GET({ url }) {
+    let version = url.searchParams.get("version")
+
+    let { data: licenses, error } = await supabase
+    .from('licenses')
+    .select('license')
+    .eq('version', version)
+
+    if(licenses) {
+        return json({
+            licenses: licenses
+        });
+    }
+
+    if(error)
+        console.log(error);
+
+    throw error(404, "Error occured");
+}
