e22654b9d59492ca8e450d98d7c5e1167c3ab900
Eddie <edpoulsonv@hotmail.com>
2023-07-17 14:15:41 -0700

    fix: version validation for auth endpoint

--- src/routes/api/ext-auth/+server.js
+++ src/routes/api/ext-auth/+server.js
@@ -1,7 +1,8 @@
-export async function GET({ locals }) {
+export async function GET({ locals, url }) {
     const session = await locals.getSession();
+    const ext_version = url.searchParams.get('ext_version')
 
-    if(session)
+    if(session && ext_version > "1.6.4")
         return new Response(JSON.stringify({ message: "authorized"}, {status: 200}))
     else
         return new Response(JSON.stringify({ message: "not authorized"}, {status: 400}))
