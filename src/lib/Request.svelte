779fec05b1f1debac44161c261faf6f8262cdfa0
Eddie <edpoulsonv@hotmail.com>
2023-07-28 15:46:31 -0700

    fix: small styling change on request component

--- src/lib/Request.svelte
+++ src/lib/Request.svelte
@@ -6,8 +6,8 @@
 
 </script>
 
-<div class="flex flex-col text-center [overflow-wrap:anywhere] flex-wrap border-2 border-black rounded-md font-mono p-1">
+<div class="flex flex-col text-center [overflow-wrap:anywhere] flex-wrap border-2 border-black rounded-md font-mono p-1 min-h-52">
     <button class="transition hover:-translate-y-2 hover:opacity-50 duration-300 py-1">^^^ {votes} ^^^</button>
-    <p class="border-2 border-gray-400 rounded-md p-1 min-w-0">{feature}</p>
-    <p class="font-semibold text-lg">{request_by}</p>
+    <p class="border-2 border-gray-400 rounded-md p-1 min-w-0 max-h-32 overflow-auto h-32">{feature}</p>
+    <p class="font-semibold text-lg mt-auto">{request_by}</p>
 </div>
